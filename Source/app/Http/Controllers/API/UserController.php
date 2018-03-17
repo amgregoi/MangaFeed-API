2f9460ea5a92769ac386cf58229035bfd3646f4f
Andy Gregoire <amgregoi08@gmail.com>
2018-03-17 03:34:21 -0500

    hotfix post create user

--- Source/app/Http/Controllers/API/UserController.php
+++ Source/app/Http/Controllers/API/UserController.php
@@ -42,7 +42,7 @@ class UserController extends Controller
             $lUser = User::where(['email' => $lEmail])->first();
             if ($lUser)
             {
-                return response()->json(['message' => "User with that email already exists"], 404);
+                return response()->json(['user' => json_encode($lUser)], 200);
             }
 
             $lUser = User::create([
